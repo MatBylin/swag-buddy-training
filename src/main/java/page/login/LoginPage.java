ae26e10a099cb2d49211037013ad8b2baed261dd
rafslaw <slawinski.rafau@gmail.com>
2025-12-02 16:23:44 +0100

    Exercise 1: Implementation of the LoginPage

--- src/main/java/page/login/LoginPage.java
+++ src/main/java/page/login/LoginPage.java
@@ -14,4 +14,24 @@ public class LoginPage extends BasePage {
 
     @FindBy(id = "user-name")
     private WebElement userNameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    private void fillUserName(String username) {
+        userNameInput.sendKeys(username);
+    }
+
+    private void fillPassword(String password) {
+        passwordInput.sendKeys(password);
+    }
+
+    public void logIn(String username, String password) {
+        fillUserName(username);
+        fillPassword(password);
+        loginButton.click();
+    }
 }
